cf994909a4929ffbb996e710b73c76982619201a
JoseMarulanda <josejumarumol@gmail.com>
2020-01-27 21:57:13 -0500

    pep fix

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -5,21 +5,24 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Display Hello HBNB """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Display HBNB """
-    return 'HBNB'       
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Display C and a text"""
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -29,10 +32,12 @@ def py_text(text='is cool'):
         text = text.replace('_', ' ')
     return 'python %s' % text
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def num(n):
     """Display a number"""
     return '%d is a number' % n
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
@@ -40,5 +45,5 @@ def number_template(n):
     return render_template('5-number.html', number=n)
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
