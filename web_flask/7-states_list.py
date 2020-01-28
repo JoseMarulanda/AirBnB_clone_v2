8d02f4c72c5acb9e956c2637c0c9942fa96d6c16
JoseMarulanda <josejumarumol@gmail.com>
2020-01-27 22:25:20 -0500

    whats the error

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,27 +1,27 @@
 #!/usr/bin/python3
-""" script that starts a Flask web application """
-
+""" Script that runs an app with Flask framework """
 from flask import Flask, render_template
 from models import storage
 from models.state import State
+
 
 app = Flask(__name__)
 
 
 @app.teardown_appcontext
-def teardown:
-    """close the strorage"""
-    storage.close
+def teardown_session(exception):
+    """ Teardown """
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
-def states_list(n):
-    """Display a html page"""
+def display_html():
+    """ Function called with /states_list route """
     states = storage.all(State)
-    dict_to_html = {value.id: vadlue.name for value in states.values()}
+    dict_to_html = {value.id: value.name for value in states.values()}
     return render_template('7-states_list.html',
-                           Table="states", items=dict_to_html)
+                           Table="States",
+                           items=dict_to_html)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
