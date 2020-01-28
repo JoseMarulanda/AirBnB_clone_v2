1fd64990361dec707ce358235157a4df9615906e
JoseMarulanda <josejumarumol@gmail.com>
2020-01-27 21:51:31 -0500

    fix pep

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -5,21 +5,24 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     """ Display Hello HBNB """
     return "Hello HBNB!"
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """ Display HBNB """
-    return 'HBNB'       
+    return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """Display C and a text"""
     return 'C %s' % text.replace('_', ' ')
+
 
 @app.route('/python/', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
@@ -29,11 +32,12 @@ def py_text(text='is cool'):
         text = text.replace('_', ' ')
     return 'python %s' % text
 
+
 @app.route('/number/<int:n>', strict_slashes=False)
 def num(n):
     """Display a number"""
     return '%d is a number' % n
 
 
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
