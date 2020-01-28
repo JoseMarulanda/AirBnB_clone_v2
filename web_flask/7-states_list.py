51cf39b580b475899d7d3087c83156e76e973ec1
JoseMarulanda <josejumarumol@gmail.com>
2020-01-27 22:09:48 -0500

    pepfix

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -7,21 +7,21 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
 def teardown:
     """  close the strorage  """
     storage.close
-    
+
+
 @app.route('/states_list', strict_slashes=False)
 def states_list(n):
     """Display a html page"""
     states = storage.all(State)
     dict_to_html = {value.id: vadlue.name for value in states.values()}
     return render_template('7-states_list.html',
-                           Table="states", items=dict_to_html) 
-    
+                           Table="states", items=dict_to_html)
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
